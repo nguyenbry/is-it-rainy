86dbf327df27a2bfdfa43a754ff27a8328ae3d92
nguyenbry <nguyen@nearmissmgmt.com>
2024-04-29 14:36:29 -0400

    setting up environment variable for API key

--- src/env.js
+++ src/env.js
@@ -8,6 +8,11 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    OPENWEATHER_KEY: z
+      .string({
+        required_error: "OPENWEATHER_KEY is required and must be a string.",
+      })
+      .min(1),
   },
 
   /**
@@ -26,6 +31,7 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    OPENWEATHER_KEY: process.env.OPENWEATHER_KEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
